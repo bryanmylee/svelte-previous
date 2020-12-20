d55371a146bfa1d8ce3a555868c036a1de75c88c
Bryan Lee <bryanmylee@gmail.com>
2020-12-20 19:20:41 +0800

    feat: implement with previous

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+import { writable } from 'svelte/store';
+
+interface WithPrevious<T> {
+  current: T,
+  previous: T | null,
+}
+
+export function withPrevious<T>(initValue: T) {
+  const values = writable<WithPrevious<T>>({
+    current: initValue,
+    previous: null,
+  });
+  return {
+    subscribe: values.subscribe,
+    setCurrent: (newValue: T) => {
+      values.update(old => ({
+        current: newValue,
+        previous: old.current,
+      }));
+    },
+  };
+}
 
