713683eaddd2488c252b8d617f3ea06ac7616e83
Bryan Lee <bryanmylee@gmail.com>
2020-12-20 20:37:15 +0800

    feat!: attempt to generate dynamic number of previous elements
    [BREAKING]

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,41 @@
 import { writable, derived, Writable, Readable } from 'svelte/store';
 
-interface WithPrevious<T> {
-  current: T,
-  previous: T | null,
-}
-
+type NonNullFirstArray<T> = [T, ...(T|null)[]];
 type Updater<T> = (toUpdate: T) => T;
 
-export function withPrevious<T>(initValue: T):
-    [Writable<T>, Readable<T | null>] {
-  const values = writable<WithPrevious<T>>({
-    current: initValue,
-    previous: null,
-  });
+export function withPrevious<T>(initValue: T, numToTrack: number = 2):
+    [Writable<T>, ...Readable<T|null>[]] {
+  if (numToTrack < 2) {
+    throw new Error('Must track a minimum of 2 versions');
+  }
+  // Generates an array of size numToTrack with the first element set to
+  // initValue and all other elements set to null.
+  const init: NonNullFirstArray<T>
+      = [initValue, ...Array(numToTrack - 1).fill(null)];
+  console.log(init);
+  const values = writable<NonNullFirstArray<T>>(init);
   const updateCurrent = (fn: Updater<T>) => {
     values.update(old => {
-      const newValue = fn(old.current);
-      return {
-        current: newValue,
-        previous: old.current,
-      };
+      const newValue = fn(old[0]);
+      // Adds the new value to the front of the array and removes the oldest
+      // value from the end.
+      return [
+        newValue,
+        ...old.slice(0, numToTrack - 1),
+      ];
     });
   }
   const current: Writable<T> = {
-    subscribe: derived(values, $values => $values.current).subscribe,
+    subscribe: derived(values, $values => $values[0]).subscribe,
     update: updateCurrent,
     set: (newValue) => {
       updateCurrent(() => newValue);
     },
   };
-  const previous = derived(values, $values => $values.previous);
-  return [current, previous];
+  // Create an array of derived stores for every other element in the array.
+  const others = Array(numToTrack - 1).map((_, i) => {
+    return derived(values, $values => $values[i + 1]);
+  });
+  return [current, ...others];
 }
 
