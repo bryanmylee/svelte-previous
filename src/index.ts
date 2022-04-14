fdf99a5a2e0f77a15db75ee20ab177a35ad44563
Bryan Lee <bryanmylee@gmail.com>
2022-04-14 23:49:47 +0800

    type: Tuple type for strict return length

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
 import { writable, derived } from 'svelte/store';
 import type { Writable, Readable, Updater } from 'svelte/store';
 
-interface WithPreviousOptions<T> {
-	numToTrack?: number;
-	initPrevious?: T[];
+type WithPreviousOptions<T, N extends number> = {
+	numToTrack?: N;
+	initPrevious?: Array<T>;
 	requireChange?: boolean;
 	isEqual?: IsEqual<T>;
-}
-type IsEqual<T> = (a: T, b: T) => boolean;
-type NonNullFirstArray<T> = [T, ...(T | null)[]];
+};
 
-export function withPrevious<T>(
+type WithPreviousResult<T, N extends number> = [
+	Writable<T>,
+	...Tuple<Readable<T | null>, N>
+];
+
+export function withPrevious<T, N extends number = 1>(
 	initValue: T,
 	{
-		numToTrack = 1,
+		numToTrack = 1 as N,
 		initPrevious = [],
 		requireChange = true,
 		isEqual = (a, b) => a === b,
-	}: Partial<WithPreviousOptions<T>> = {}
-): [Writable<T>, ...Readable<T | null>[]] {
+	}: WithPreviousOptions<T, N> = {}
+): WithPreviousResult<T, N> {
 	if (numToTrack <= 0) {
 		throw new Error('Must track at least 1 previous');
 	}
 
 	// Generates an array of size numToTrack with the first element set to
 	// initValue and all other elements set to ...initPrevious or null.
-	const rest: (T | null)[] = initPrevious.slice(0, numToTrack);
+	const rest: Array<T | null> = initPrevious.slice(0, numToTrack);
 	while (rest.length < numToTrack) {
 		rest.push(null);
 	}
@@ -55,10 +58,55 @@ export function withPrevious<T>(
 	const others = [...Array(numToTrack)].map((_, i) =>
 		derived(values, ($values) => $values[i + 1])
 	);
-	return [current, ...others];
+	return [current, ...others] as WithPreviousResult<T, N>;
 }
 
 /**
  * @deprecated Since version 2.0.1. Use `withPrevious` instead.
  */
 export const usePrevious = withPrevious;
+
+// UTILITY TYPES
+// =============
+type IsEqual<T> = (a: T, b: T) => boolean;
+type NonNullFirstArray<T> = [T, ...Array<T | null>];
+
+/**
+ * Adopted from https://github.com/microsoft/TypeScript/issues/26223#issuecomment-674514787
+ */
+type BuildPowersOf2LengthArrays<
+	N extends number,
+	R extends never[][]
+> = R[0][N] extends never
+	? R
+	: BuildPowersOf2LengthArrays<N, [[...R[0], ...R[0]], ...R]>;
+
+type ConcatLargestUntilDone<
+	N extends number,
+	R extends never[][],
+	B extends never[]
+> = B['length'] extends N
+	? B
+	: [...R[0], ...B][N] extends never
+	? ConcatLargestUntilDone<
+			N,
+			R extends [R[0], ...infer U] ? (U extends never[][] ? U : never) : never,
+			B
+	  >
+	: ConcatLargestUntilDone<
+			N,
+			R extends [R[0], ...infer U] ? (U extends never[][] ? U : never) : never,
+			[...R[0], ...B]
+	  >;
+
+type Replace<R extends unknown[], T> = { [K in keyof R]: T };
+
+type Tuple<T, N extends number> = number extends N
+	? T[]
+	: {
+			[K in N]: BuildPowersOf2LengthArrays<K, [[never]]> extends infer U
+				? U extends never[][]
+					? Replace<ConcatLargestUntilDone<K, U, []>, T>
+					: never
+				: never;
+	  }[N];
